8d026a97bf5f0f78d87c91f410c8ed6338d8ab6e
Fredrik Johnsen <fredrik@memosoft.no>
2024-02-05 20:00:32 +0100

    Initial commit

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-slate-800">
+    <div class="container mx-auto h-screen text-center text-slate-300 p-12">
+      <img
+        class="p-10 max-h-96 mx-auto"
+        src="assets/fredd-icon.png"
+        alt="Discover Fredd"
+      />
+      <h1 class="font-thin text-4xl pt-12">Fredd.no</h1>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare-pages'
+    preset: 'cloudflare-pages',
   },
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+});
